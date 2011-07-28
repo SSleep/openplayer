1e50edbb1de96dc852e3855a8d65050614088f37
Dmitry Savchenko <login.was.here@gmail.com>
2011-07-28 22:35:47 +0300

    Fix for delete track from playlist functionality

--- managers/Playlist.php
+++ managers/Playlist.php
@@ -7,25 +7,25 @@ class Playlist extends \Lib\Base\Manager {
         if ( !$user ) {
             return array();
         }
-        
+
         $userId = intval($user->id);
-        
+
         $q = "SELECT * FROM pl WHERE userId = {$userId}";
         $res = $this->pdo->query( $q );
 
         if ( !$res ) {
             return array();
         }
-        
+
         return $res->fetchAll( \PDO::FETCH_OBJ );
     }
 
     public function getSongs( $plId ) {
         $plId = intval($plId);
-        
+
         $user = User::getUser();
         $userId = intval($user->id);
-        
+
         $q = "SELECT * FROM pl INNER JOIN pl_song pls ON pl.id = pls.plId WHERE pl.userId = {$userId} AND pl.id = {$plId} ORDER BY pls.position";
 
         $res = $this->pdo->query( $q );
@@ -47,27 +47,27 @@ class Playlist extends \Lib\Base\Manager {
 
     private function checkIfMine( $id ) {
         $id = intval($id);
-        
+
         $user = User::getUser();
         $userId = intval($user->id);
-        
-        
+
+
         $q = "SELECT * FROM pl WHERE id = {$id} AND userId = {$userId}";
         $res = $this->pdo->query( $q );
-        
+
         if ( $res->fetchObject() ) {
             return true;
         }
-        
+
         return false;
     }
-    
+
     public function delPL( $id ) {
         if ( !$this->checkIfMine( $id ) ) {
             return false;
         }
         $id = intval($id);
-        
+
         $q = "DELETE FROM pl_song WHERE plId = {$id}";
         $this->pdo->exec( $q );
 
@@ -79,12 +79,12 @@ class Playlist extends \Lib\Base\Manager {
         if ( !$this->checkIfMine( $id ) ) {
             return false;
         }
-        
+
         $id = intval($id);
-        
+
         $user = User::getUser();
         $userId = intval($user->id);
-        
+
         $res = $this->pdo->prepare("UPDATE pl SET name = ? WHERE id = ? AND userId = ?");
         $res->execute(array($name, $id, $userId));
         return $res;
@@ -95,29 +95,29 @@ class Playlist extends \Lib\Base\Manager {
             return false;
         }
         $plId = intval($plId);
-        
+        $id = $this->pdo->quote($id);
+
         $pos = $this->getSongPosition( $id, $plId );
         $this->downPositions( $pos, $plId );
-        
-        $id = $this->pdo->quote($id);
+
         $plId = intval($plId);
-        
+
         $q = "DELETE FROM pl_song WHERE plId = {$plId} AND songId = {$id}";
         return $this->pdo->exec( $q );
     }
-    
+
     private function downPositions( $afterPosition, $plId ) {
         $plId = intval($plId);
         $afterPosition = intval($afterPosition);
-        
+
         $q = "UPDATE pl_song SET position = position - 1 WHERE plId = {$plId} AND position > {$afterPosition}";
         $this->pdo->exec( $q );
     }
-    
+
     private function upPositions( $afterPosition, $plId ) {
         $plId = intval($plId);
         $afterPosition = intval($afterPosition);
-        
+
         $q = "UPDATE pl_song SET position = position + 1 WHERE plId = {$plId} AND position > {$afterPosition}";
         $this->pdo->exec( $q );
     }
@@ -130,19 +130,19 @@ class Playlist extends \Lib\Base\Manager {
         if ( !$this->checkIfMine( $toId ) || ($fromId && !$this->checkIfMine( $fromId )) ) {
             return false;
         }
-        
+
         # positioning
         if ( $fromId ) {
             $oldPosition = $this->getSongPosition( $songData['id'], $fromId );
             $this->downPositions( $oldPosition, $fromId );
         }
-        
+
         $newPosition = 1;
         if ( $afterId ) {
             $newPosition = $this->getSongPosition( $afterId, $toId );
             $newPosition++;
         }
-        
+
         $this->upPositions( $newPosition-1, $toId );
         # /positioning
 
@@ -150,7 +150,7 @@ class Playlist extends \Lib\Base\Manager {
             $songInfo = serialize( $songData );
             $songInfo = $this->pdo->quote($songInfo);
             $songInfo = strip_tags($songInfo);
-            
+
             $q = "INSERT INTO pl_song VALUES (null, '{$songData['id']}', $toId, {$songInfo}, {$newPosition})";
             $status = $this->pdo->exec( $q );
         } elseif( false && ($fromId != $toId) ) { //@todo
@@ -160,43 +160,43 @@ class Playlist extends \Lib\Base\Manager {
             $q = "UPDATE pl_song SET plId = {$toId}, position={$newPosition} WHERE songId={$songId} AND plId={$fromId}";
             $status = $this->pdo->exec( $q );
         }
-        
+
         return $status;
     }
-    
+
     private function getSongPosition( $songId, $plId ) {
         $plId = intval($plId);
-        
+
         $q = "SELECT * FROM pl_song WHERE songId = {$songId} AND plId = $plId";
         $res = $this->pdo->query( $q );
-        
+
         if (!$res) return 0;
-        
+
         return ($res->fetchObject()->position) * 1;
     }
-    
+
     public function updateSongInfo( $id, $songInfo ) {
         $id = $this->pdo->quote($id);
-        
+
         $q = "SELECT * FROM pl_song WHERE songId = {$id}";
         $res = $this->pdo->query($q);
-        
+
         foreach ( $res->fetchAll( \PDO::FETCH_OBJ ) as $song) {
             $songInfo = array_merge(
                 unserialize($song->songInfo),
                 $songInfo
             );
-            
+
             $songInfo = serialize($songInfo);
             $songInfo = $this->pdo->quote($songInfo);
             $id = $this->pdo->quote($id);
 //            $songInfo = strip_tags($songInfo);
-            
+
             $q = "UPDATE pl_song SET songInfo = {$songInfo} WHERE songId = {$id}";
             $this->pdo->exec($q);
         }
-        
+
         return true;
     }
-    
+
 }
